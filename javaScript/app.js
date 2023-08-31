ceca4d002057e66bdd8505f2687fd9d2da57bf70
Muhammad Owais <mowaisabbas9@gmail.com>
2023-08-31 06:42:45 +0500

    update profile , updete password ,post blog , filter blog,upload image on storage

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,63 +1,180 @@
 import {
-    createUserWithEmailAndPassword, auth, db, doc, setDoc, signInWithEmailAndPassword, onAuthStateChanged, signOut, ref,
-    uploadBytesResumable, getDownloadURL, storage
+    createUserWithEmailAndPassword, auth, db, doc, getDoc, setDoc, signInWithEmailAndPassword, onAuthStateChanged, signOut, ref,
+    uploadBytesResumable, getDownloadURL, storage, updateDoc, updatePassword, collection, addDoc, serverTimestamp, query, where, getDocs
 } from './firebase.js'
 
 
 const loader = document.getElementById("loader-div");
 const inputs = document.querySelectorAll("input");
 
+
 // ======================================================================= /
-// ==========================Statecahnged Function======================== /
+// ====================== Get Specific User Blogs ======================== /
 // ======================================================================= /
-onAuthStateChanged(auth, (user) => {
-    if (user) {
+const getSpUserBlog = async () => {
+    loader.style.display = "grid"
+    const urlSrch = new URLSearchParams(location.search);
+    const user = urlSrch.get('user');
+    const q = query(collection(db, "blogs"), where("uid", "==", user));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        let heading = document.getElementById("heading");
+        heading.innerHTML = `All blogs from ${doc.data().userInfo.FirstName}`
 
-        if (location.pathname === "/index.html") {
+        const allBlogsSpcUser = document.getElementById("allBlogsSpcUser");
+        allBlogsSpcUser.innerHTML += `<div class="card col-12 ps-2 mb-4">
+        <div class="card-body">
+        <div class="d-flex ">
+        <img id="blogImage" src="${doc.data().userInfo.Image}" alt="User Profile">
+        <div class="ms-3 d-flex flex-column justify-content-center">
+        <h5 class="card-title fs-4 col-8">${doc.data().title}</h5>
+        <div class="d-flex align-items-center">
+        
+        <h6 class="card-subtitle p-0 m-0 text-body-secondary">${doc.data().userInfo.FirstName} ${"&nbsp"}</h6> <span
+        id="dateInBlog"> -  ${doc.data().time.toDate().toDateString()}</span>
+        </div>
+        </div>
+        </div>
+        <p class="card-text mt-3">${doc.data().description}</p>
+        </div>
+        </div>`
+    });
 
-            let changeHref = document.querySelectorAll('.changeHref');
-            changeHref.forEach((value, index) => {
-                value.innerHTML = "Dashboard"
-                value.href = "pages/home.html"
-            })
+    loader.style.display = "none"
 
+
+};
+
+
+
+// ======================================================================= /
+// ========================== Get User Blogs ============================= /
+// ======================================================================= /
+
+const getAllBlogs = async () => {
+    loader.style.display = "grid"
+
+    const querySnapshot = await getDocs(collection(db, "blogs"));
+    querySnapshot.forEach((doc) => {
+        // console.log(doc.id, "------ => ", doc.data());
+        let allBlogsHere = document.getElementById("allBlogsHere");
+
+        allBlogsHere.innerHTML += `<div class="card col-12 ps-2 mb-4">
+        <div class="card-body">
+            <div class="d-flex ">
+                <img id="blogImage" src="${doc.data().userInfo.Image} " alt="User Profile">
+                <div class="ms-3 d-flex flex-column justify-content-center">
+                    <h5 class="card-title fs-4 col-8">${doc.data().title}</h5>
+                    <div class="d-flex align-items-center">
+
+                        <h6 class="card-subtitle p-0 m-0 text-body-secondary">${doc.data().userInfo.FirstName} ${"&nbsp"} </h6> <span
+                            id="dateInBlog"> -   ${doc.data().time.toDate().toDateString()}</span>
+                    </div>
+                </div>
+            </div>
+            <p class="card-text mt-3">${doc.data().description}</p>
+            <a href="pages/user.html?user=${doc.data().uid}" class="card-link">See all blogs from this user</a>
+        </div>
+    </div>`
+    });
+    loader.style.display = "none"
+
+};
+
+
+
+
+
+// ======================================================================= /
+// ========================== Get User Blogs ============================= /
+// ======================================================================= /
+
+const getBlogs = async (user) => {
+    loader.style.display = "grid"
+    const q = query(collection(db, "blogs"), where("uid", "==", user.uid));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+
+        // console.log(doc.id, " => ", doc.data().title);
+        let blogHere = document.getElementById("blogHere");
+        blogHere.innerHTML += ` <div class="card col-12 ps-2 mb-4">
+             <div class="card-body">
+                 <div class="d-flex ">
+                     <img id="blogImage" src="${doc.data().userInfo.Image}" alt="User Profile">
+                     <div class="ms-3 d-flex flex-column justify-content-center">
+                         <h5 class="card-title fs-4 col-8 col-sm-10">${doc.data().title}</h5>
+                         <div class="d-flex align-items-center">
+                             <h6 class="card-subtitle p-0 m-0 text-body-secondary">${doc.data().userInfo.FirstName} ${"&nbsp"} </h6>  <span id="dateInBlog"> - ${doc.data().time.toDate().toDateString()}</span>
+                         </div>
+                     </div>
+                 </div>
+                 <p class="card-text mt-3">${doc.data().description}</p>
+                 <a href="" id='${doc.id}' class="card-link">Delete</a>
+                 <a href="" class="card-link">Edit</a>
+             </div>
+         </div>`
+    });
+    loader.style.display = "none"
+
+};
+
+// ======================================================================= /
+// ============================== Get Data =============================== /
+// ======================================================================= /
+const getData = async (user) => {
+    const docRef = doc(db, "user", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        let profileImg = document.getElementById("profileImg");
+        if (location.pathname === "/pages/profile.html") {
+            inputs[1].value = docSnap.data().FirstName;
+            inputs[2].value = docSnap.data().Password;
+            profileImg.src = docSnap.data().Image;
         }
-
-        const uid = user.uid;
-        if (location.pathname !== "/pages/home.html" && location.pathname !== "/pages/user.html" && location.pathname !== "/index.html" && location.pathname !== "/pages/profile.html") {
-            location.href = "home.html"
+        if (location.pathname === "/pages/home.html") {
+            let [a, b] = document.getElementsByClassName('dashboardUserName')
+            a.innerHTML = docSnap.data().FirstName;
+            b.innerHTML = docSnap.data().FirstName;
         }
     } else {
-        if (location.pathname !== "/pages/login.html" && location.pathname !== "/pages/signup.html" && location.pathname !== "/index.html" && location.pathname !== "/pages/user.html") {
-            location.href = "login.html"
-        }
-
+        console.log("No such document!");
     }
-});
+    if (location.pathname === "/index.html" || location.pathname === "/pages/user.html") {
+        let changeHref = document.querySelectorAll('.changeHref');
+        changeHref.forEach((value, index) => {
+            value.innerHTML = "Dashboard"
+            value.href = "pages/home.html"
+        })
+    }
+}
 
 
 // ======================================================================= /
 // ==========================Signup Users================================= /
 // ======================================================================= /
 const signupBtn = document.getElementById("signupBtn");
-signupBtn && signupBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+signupBtn && signupBtn.addEventListener('click', () => {
     if (inputs[3].value === inputs[4].value) {
 
         loader.style.display = "grid"
         createUserWithEmailAndPassword(auth, inputs[2].value, inputs[4].value)
             .then(async (userCredential) => {
+                try {
 
-                const user = userCredential.user;
-                console.log(":user===>", user)
-                await setDoc(doc(db, "user", user.uid), {
-                    FirstName: inputs[0].value,
-                    LastName: inputs[1].value,
-                    Email: inputs[2].value,
-                    Password: inputs[3].value,
-                    ConfPassword: inputs[4].value
-                });
-                loader.style.display = "none"
+                    const user = userCredential.user;
+                    console.log(":user===>", user)
+                    await setDoc(doc(db, "user", user.uid), {
+                        FirstName: inputs[0].value,
+                        LastName: inputs[1].value,
+                        Email: inputs[2].value,
+                        Password: inputs[3].value,
+                        ConfPassword: inputs[4].value
+                    });
+                    loader.style.display = "none"
+                    location.href = "../index.html"
+                } catch (error) {
+                    console.log(error, "wwwwwerdjfkds")
+                }
 
             })
             .catch((error) => {
@@ -79,6 +196,182 @@ signupBtn && signupBtn.addEventListener('click', (e) => {
     }
 
 });
+
+
+// ======================================================================= /
+// ==========================Statecahnged Function======================== /
+// ======================================================================= /
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const uid = user.uid;
+
+        if (location.pathname !== "/pages/home.html" && location.pathname !== "/pages/user.html" && location.pathname !== "/index.html" && location.pathname !== "/pages/profile.html") {
+            location.href = "/pages/home.html"
+        }
+        console.log(user)
+        getData(user)
+        if (location.pathname === "/pages/home.html") {
+            getBlogs(user)
+        }
+        else if (location.pathname === "/index.html") {
+            getAllBlogs()
+        }
+        else if (location.pathname === "/pages/user.html") {
+            getSpUserBlog()
+        }
+
+    } else {
+        if (location.pathname === "/index.html") {
+
+            getAllBlogs()
+        }
+        else if (location.pathname === "/pages/user.html") {
+
+            getSpUserBlog()
+        }
+        if (location.pathname !== "/pages/login.html" && location.pathname !== "/pages/signup.html" && location.pathname !== "/index.html" && location.pathname !== "/pages/user.html") {
+            location.href = "/index.html"
+        }
+
+    }
+});
+
+
+// ======================================================================= /
+// =========================== Add Image ================================= /
+// ======================================================================= /
+
+
+const uploadFile = (file) => {
+
+    const user = auth.currentUser;
+    // console.log("filename", file.name)
+    return new Promise((resolve, reject) => {
+        const imgRef = ref(storage, `images/${user.uid}`);
+        const uploadTask = uploadBytesResumable(imgRef, file);
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                // console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        // console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        // console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                reject(error)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL);
+                    // console.log(downloadURL)
+                });
+            }
+        );
+    })
+}
+let srrc;
+
+const fileInput = document.querySelectorAll("input[type='file']")[0];
+fileInput && fileInput.addEventListener("change", () => {
+    loader.style.display = "grid"
+
+    // console.log("working", fileInput.files[0])
+
+    uploadFile(fileInput.files[0]).then(async (res) => {
+
+        let profileImg = document.getElementById("profileImg");
+        profileImg.src = res;
+        srrc = res;
+        // console.log("imgurl", res)
+        loader.style.display = "none"
+
+    }).catch((rej) => {
+        // console.log("reject==>".rej)
+        loader.style.display = "none"
+
+    })
+});
+
+
+
+// ======================================================================= /
+// ==========================Update Users================================= /
+// ======================================================================= /
+
+let userNameUpdate = document.getElementById('userNameUpdate');
+const changeUserName = document.getElementById('changeUserName');
+changeUserName && changeUserName.addEventListener('click', () => {
+    inputs[1].disabled = false;
+});
+
+const updateBtn = document.getElementById('updateBtn');
+updateBtn && updateBtn.addEventListener("click", async () => {
+    const user = auth.currentUser
+
+    loader.style.display = "grid"
+    inputs[1].disabled = true;
+    // ================== Update Password ==================//
+    let newPassword;
+    if (inputs[3].value === inputs[4].value) {
+        newPassword = inputs[3].value;
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Both passwords are different'
+        })
+        loader.style.display = "none"
+
+    }
+
+    updatePassword(user, newPassword).then(() => {
+        // console.log("password is update")
+
+        loader.style.display = "none"
+        Swal.fire({
+            icon: 'success',
+            title: 'Updated',
+            text: 'Your profile is update'
+        })
+    }).catch((error) => {
+        loader.style.display = "none"
+        // console.log("password is update", error)
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Some Errors'
+        })
+    });
+
+
+    //===============
+    let obj = {
+        FirstName: inputs[1].value,
+        Password: inputs[3].value
+    }
+    if (srrc) {
+
+        obj.Image = srrc
+    }
+    const washingtonRef = doc(db, "user", user.uid);
+    await updateDoc(washingtonRef,
+        obj
+    );
+
+
+    loader.style.display = "none"
+    inputs[3].value = ""
+    inputs[4].value = ""
+    // console.log("chal gaya reeee")
+})
+
+
 // ======================================================================= /
 // ==========================Login Users================================= /
 // ======================================================================= /
@@ -96,7 +389,7 @@ loginBtn && loginBtn.addEventListener("click", (e) => {
         .then((userCredential) => {
 
             const user = userCredential.user;
-            console.log(user)
+            // console.log(user)
             loader.style.display = "none"
 
         })
@@ -115,7 +408,7 @@ loginBtn && loginBtn.addEventListener("click", (e) => {
 });
 
 // ======================================================================= /
-// ==========================Signup Users================================= /
+// ==========================Logout Users================================= /
 // ======================================================================= /
 const logoutUser = document.querySelectorAll(".logoutUser");
 
@@ -133,85 +426,32 @@ logoutUser && logoutUser.forEach((v, i) => {
     })
 
 });
+
 // ======================================================================= /
 // ========================== Post Blogs ================================= /
 // ======================================================================= /
 
-// const publishBlog = document.getElementById("publishBlog");
-// publishBlog && publishBlog.addEventListener("click", () => {
-//     const titleInput = document.getElementById("titleInput");
-//     const desInput = document.getElementById("desInput");
-
-//     set(ref(database, 'users/' + "hhhhhrrrrrr"), {
-//         title: titleInput.value,
-//         description: desInput.value
-//     });
-
-
-
-// });
-// ======================================================================= /
-// =========================== Add Image ================================= /
-// ======================================================================= /
-
-
-
-
-const uploadFile = (file) => {
-
-    const user = auth.currentUser
-    console.log("filename", file.name)
-    return new Promise((resolve, reject) => {
-        const imgRef = ref(storage, `images/${user.uid}`);
-        const uploadTask = uploadBytesResumable(imgRef, file);
-        uploadTask.on('state_changed',
-            (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case 'paused':
-                        console.log('Upload is paused');
-                        break;
-                    case 'running':
-                        console.log('Upload is running');
-                        break;
-                }
-            },
-            (error) => {
-                reject(error)
-            },
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    resolve(downloadURL);
-                    console.log(downloadURL)
-                });
-            }
-        );
-    })
-}
-let srrc;
-
-const fileInput = document.querySelectorAll("input[type='file']")[0];
-fileInput && fileInput.addEventListener("change", () => {
+const publishBlog = document.getElementById("publishBlog");
+publishBlog && publishBlog.addEventListener("click", async () => {
     loader.style.display = "grid"
 
-    console.log("working", fileInput.files[0])
+    const user = auth.currentUser;
+    const userRef = doc(db, "user", user.uid);
+    const docSnap = await getDoc(userRef);
 
-    uploadFile(fileInput.files[0]).then(async(res) => {
-       
-        let profileImg = document.getElementById("profileImg");
-        profileImg.src = res;
-        srrc = res;
-        console.log("imgurl", res)
-        loader.style.display = "none"
-
-    }).catch((rej) => {
-        console.log("reject==>".rej)
-        loader.style.display = "none"
-
-    })
+    let titleInput = document.getElementById("titleInput");
+    let desInput = document.getElementById("desInput");
+    const docRef = await addDoc(collection(db, "blogs"), {
+        title: titleInput.value,
+        description: desInput.value,
+        time: serverTimestamp(),
+        uid: user.uid,
+        userInfo: docSnap.data()
+    });
+    loader.style.display = "none"
+    titleInput.value = ""
+    desInput.value = ""
+    window.location.reload()
 });
-
-
 
 
--- javaScript/firebase.js
+++ javaScript/firebase.js
@@ -1,24 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updatePassword } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc,collection, addDoc, serverTimestamp , query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
 // import { getDatabase, ref, set } from"https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-storage.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-storage.js";
 
- 
- const firebaseConfig = {
-   apiKey: "AIzaSyD7x-hnLRut59WaCA4SiN_HGBqbnpNngZI",
-   authDomain: "blog-app-a8820.firebaseapp.com",
-   projectId: "blog-app-a8820",
-   storageBucket: "blog-app-a8820.appspot.com",
-   messagingSenderId: "306176913336",
-   appId: "1:306176913336:web:9a8b9677d09acaa0eda94d"
- };
- 
- const app = initializeApp(firebaseConfig);
- const auth = getAuth(app);
- const db = getFirestore(app);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7x-hnLRut59WaCA4SiN_HGBqbnpNngZI",
+  authDomain: "blog-app-a8820.firebaseapp.com",
+  projectId: "blog-app-a8820",
+  storageBucket: "blog-app-a8820.appspot.com",
+  messagingSenderId: "306176913336",
+  appId: "1:306176913336:web:9a8b9677d09acaa0eda94d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 //  const database = getDatabase(app);
- const storage = getStorage(app);
+const storage = getStorage(app);
 
- export { createUserWithEmailAndPassword ,auth ,db ,doc, setDoc , signInWithEmailAndPassword, onAuthStateChanged, signOut, ref,
-   uploadBytesResumable, getDownloadURL,storage}
+export {
+  createUserWithEmailAndPassword, auth, db, doc, getDoc, setDoc, signInWithEmailAndPassword, onAuthStateChanged, signOut, ref,
+  uploadBytesResumable, getDownloadURL, storage, updateDoc, updatePassword,collection, addDoc, serverTimestamp, query, where, getDocs
+}
